ee526715f9b1e3cd74eb63b44184850a064865c1
raniel01 <38967359+raniel01@users.noreply.github.com>
2019-07-31 18:37:24 -0300

    exercicio 6 - criação das classes veiculo e carro

---
+++ listaExercicio6/listaExercicio6/Carro.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace listaExercicio6
+{
+    class Carro : Veiculo
+    {
+        //ATRIBUTOS DA CLASSE
+        private int capacidadePortaMala;
+        private bool bagageiro;
+        private string carroceria;
+
+        // CONSTRUTOR SEM PARÂMETROS
+        public Carro ()
+            : base()
+        {
+            capacidadePortaMala = 0;
+            bagageiro = false;
+            carroceria = null;
+        }
+
+        // CONSTRUTOR COM PARÂMETROS
+        public Carro(string modelo, string fabricante, int ano, string cor, int numero_portas, int capacidadePortaMala, bool bagageiro, string carroceria)
+            : base(modelo, fabricante, ano, cor, numero_portas)
+        {
+            this.capacidadePortaMala = capacidadePortaMala;
+            this.bagageiro = bagageiro;
+            this.carroceria = carroceria;
+        }
+        //PROPRIEDADES DO CAMPO PARA ACESSO AO SEU CONTEÚDO
+        public int CapacidadePotaMala
+        {
+            get { return capacidadePortaMala; }
+            set { capacidadePortaMala = value; }
+        }
+        public bool Bagageiro
+        {
+            get { return bagageiro; }
+            set { bagageiro = value; }
+        }
+        public string Carroceria
+        {
+            get { return carroceria; }
+            set { carroceria = value; }
+        }
+        //MÉTODO PARA RETORNAR UMA STRING CONTENDO AS INFORMAÇÕES DO VEICULO
+
+        public override string ToString()
+        {
+            return (String.Format(" Capacidade do porta mala: {0} \n Bagageiro: {1} \n Carroceria: {2} \n", base.ToString(), capacidadePortaMala, bagageiro, carroceria));
+        }
+    }
+}
--- listaExercicio6/listaExercicio6/veiculo.cs
+++ listaExercicio6/listaExercicio6/veiculo.cs
@@ -4,8 +4,68 @@ using System.Text;
 
 namespace listaExercicio6
 {
-    class veiculo
+    abstract class Veiculo
     {
+        //ATRIBUTOS DA CLASSE
+        private string modelo;
+        private string fabricante;
+        private int ano;
+        private string cor;
+        private int numero_portas;
 
+        // CONSTRUTOR SEM PARÂMETROS
+        public Veiculo()
+        {
+            this.modelo = null;
+            this.fabricante = null;
+            this.ano = 0;
+            this.cor = null;
+            this.numero_portas = 0;
+        }
+
+
+        // CONSTRUTOR COM PARÂMETROS
+        public Veiculo(string modelo, string fabricante, int ano, string cor, int numero_portas)
+        {
+            this.modelo = modelo;
+            this.fabricante = fabricante;
+            this.ano = ano;
+            this.cor = cor;
+            this.numero_portas = numero_portas;
+        }
+
+        //PROPRIEDADES DO CAMPO PARA ACESSO AO SEU CONTEÚDO
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+        public string Fabricante
+        {
+            get { return fabricante; }
+            set { fabricante = value; }
+        }
+        public int Ano
+        {
+            get { return ano; }
+            set {ano = value; }
+        }
+        public string Cor
+        {
+            get { return cor; }
+            set { cor = value; }
+        }
+        public int Numero_portas
+        {
+            get { return numero_portas; }
+            set { numero_portas = value; }
+        }
+
+        //MÉTODO PARA RETORNAR UMA STRING CONTENDO AS INFORMAÇÕES DO VEICULO
+
+        public override string ToString()
+        {
+            return (String.Format(" Modelo: {0} \n Fabricante: {1} \n Ano: {2} \n Cor: {3} \n Número de Portas: {4} \n", modelo, fabricante, ano, cor, numero_portas));
+        }
     }
 }

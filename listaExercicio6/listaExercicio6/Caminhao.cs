5010a17a5f98812a6589b61f0ad15e55f6cdf961
raniel01 <ranielsnts@hotmail.com>
2019-07-31 20:32:45 -0300

    atualização

---
+++ listaExercicio6/listaExercicio6/Caminhao.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace listaExercicio6
+{
+    class Caminhao : Veiculo
+    {
+        //ATRIBUTOS DA CLASSE
+        private int numero_eixos;
+        private double peso_maximo_carga;
+        private string tipo_caminhao;
+
+
+        // CONSTRUTOR SEM PARÂMETROS
+        public Caminhao()
+        : base ()
+        {
+            numero_eixos = 0;
+            peso_maximo_carga = 0.0;
+            tipo_caminhao = null;
+        }
+
+        // CONSTRUTOR COM PARÂMETROS
+        public Caminhao (string modelo, string fabricante, int ano, string cor, int numero_portas, int numero_eixos, double peso_maximo_carga, string tipo_caminhao)
+            : base(modelo, fabricante, ano, cor, numero_portas)
+        {
+            this.numero_eixos = numero_eixos;
+            this.peso_maximo_carga = peso_maximo_carga;
+            this.tipo_caminhao = tipo_caminhao;
+        }
+
+        //PROPRIEDADES DO CAMPO PARA ACESSO AO SEU CONTEÚDO
+        public int Numero_eixos
+        {
+            get { return numero_eixos; }
+            set { numero_eixos = value; }
+        }
+        public double Peso_maximo_carga
+        { 
+            get { return peso_maximo_carga; }
+            set { peso_maximo_carga = value; }
+        }
+        public string Tipo_caminhao
+        {
+            get { return tipo_caminhao; }
+            set {tipo_caminhao = value; }
+        }
+
+        //MÉTODO PARA RETORNAR UMA STRING CONTENDO AS INFORMAÇÕES DO VEICULO
+        public override string ToString()
+        {
+            return (String.Format(" Numero de eixos: {0} \n Peso máximo da Carga: {1} \n Tipo de caminão: {2} \n", base.ToString(), numero_eixos, peso_maximo_carga, tipo_caminhao));
+        }
+    }
+}
--- listaExercicio6/listaExercicio6/Program.cs
+++ listaExercicio6/listaExercicio6/Program.cs
@@ -6,7 +6,7 @@ namespace listaExercicio6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }

b7ff576a1cc575deae1f51c996839d7dfe73fff4
raniel01 <ranielsnts@hotmail.com>
2019-08-15 19:51:13 -0300

    Começando os exercicios do professor Augusto

---
+++ exerciciosAugusto/exercicios/exercicios/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1. Crie um vetor de 4 elementos (int), crie um método que alimente este vetor, crie um método que
+            mostre os valores deste vetor e crie um método que multiplique por um número inteiro todos os
+            valores do vetor.*/
+
+            const int tam = 4;
+
+            Numeros [] numeros = new Numeros[tam]; 
+        }
+    }
+}

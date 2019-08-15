b9a4f8ec948711bdef29da9640b7949dd19663a1
raniel01 <ranielsnts@hotmail.com>
2019-08-15 20:30:42 -0300

    Começando os exercicios do professor Augusto

--- exerciciosAugusto/exercicios/exercicios/Program.cs
+++ exerciciosAugusto/exercicios/exercicios/Program.cs
@@ -14,9 +14,62 @@ namespace exercicios
             mostre os valores deste vetor e crie um método que multiplique por um número inteiro todos os
             valores do vetor.*/
 
-            const int tam = 4;
+            
+             int[] x = new int[4];
+            //metodos
 
-            Numeros [] numeros = new Numeros[tam]; 
+            GerarVetor(x);
+            ImprimirVetor(x);
+            MultiplicarVetor(x);
+
+            Console.ReadKey();
+            
         }
+
+        // metodo para multiplicar
+
+        static void MultiplicarVetor(int[] x)
+        {
+            Console.WriteLine("Digite um numero multiplicador"); int mult = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Posição 1 do vetor: " + (x[0] * mult));
+            Console.WriteLine("Posição 2 do vetor: " + (x[1] * mult));
+            Console.WriteLine("Posição 3 do vetor: " + (x[2] * mult));
+            Console.WriteLine("Posição 4 do vetor: " + (x[3] * mult));
+
+        }
+
+        // metodo para imprimir
+        
+        static void ImprimirVetor (int[] x)
+        {
+            for ( int i = 0; i <  4; i++)
+            {
+                Console.WriteLine("Primeiro numero: " + x[i]);
+            }
+        }
+
+        // metodo para gerar
+
+        static void GerarVetor (int[] x)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (i != 0)
+                {
+                    Console.WriteLine("Digite mais um numero: "); int num = int.Parse(Console.ReadLine());
+                    x[i] = num;
+
+                }
+                else
+                {
+                    Console.WriteLine("Digite um numero: "); int num = int.Parse(Console.ReadLine());
+                    x[i] = num;
+                }
+                
+            }
+
+        }
+
     }
 }

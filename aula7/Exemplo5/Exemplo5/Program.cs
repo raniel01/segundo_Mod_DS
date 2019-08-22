edf62f68281c8a5ec468d091fc28a571584b3212
raniel01 <ranielsnts@hotmail.com>
2019-08-22 20:22:58 -0300

    Exemplo 4 e 5

---
+++ aula7/Exemplo5/Exemplo5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciando a variavel stream 
+            FileStream stream = new FileStream(@"Exemplo4.bin", FileMode.Open);
+        }
+    }
+}
---
+++ aula7/Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciando as variáveis
+            int idade = 25;
+            string nome = "Sergio";
+            bool temFilhos = false;
+            double altura = 1.79;
+
+            //criando o arquivo
+            FileStream stream = new FileStream(@"ExemploTeste.bin", FileMode.Create);
+
+            // iniciando a variável do tipo BinaryWriter
+            BinaryWriter writer = new BinaryWriter(stream);
+
+            // usando o método Write para escrever no arquivo
+            writer.Write(idade);
+            writer.Write(nome);
+            writer.Write(temFilhos);
+            writer.Write(altura);
+
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}

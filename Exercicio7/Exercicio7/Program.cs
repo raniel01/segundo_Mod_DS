5fb9e5fea5eaf3fd35b4208bcfa2427314a25b32
raniel01 <ranielsnts@hotmail.com>
2019-08-23 22:17:21 -0300

    continuando exercicio 7

--- Exercicio7/Exercicio7/Program.cs
+++ Exercicio7/Exercicio7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,48 @@ namespace Exercicio7
     {
         static void Main(string[] args)
         {
+            int op = 0;
+            do
+            {
+                op = menu(); 
+                switch (op)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Digite sua mensagem: ");
+                        string msg =  Console.ReadLine();
+                        //crindo diretário
+                        Directory.CreateDirectory(@"c:\Teste\");
+
+                        //criando o arquivo
+                        FileStream stream = new FileStream(@"c:\Teste\Exercicio2.bin", FileMode.Create);
+
+                        // instanciando a variavel do tipo BinaryWrite
+                        BinaryWriter writer = new BinaryWriter(stream);
+
+                        //usando o metodo Write para escrever no arquivo
+                        writer.Write(msg);
+
+                        writer.Flush();
+                        writer.Close();
+
+                        break;
+                    case 2:  break;
+                    case 0:  break;
+                    default: Console.WriteLine("Opção Ivalida"); break;
+                }
+                Console.ReadKey();
+            } while (op != 0);
+
+        }
+        public static int menu()
+        {
+            Console.Clear();
+            Console.WriteLine("*****MENU PRINCIPAL*****\n");
+            Console.WriteLine("1 - Escrever uma mensagem.");
+            Console.WriteLine("2 - Leia uma mensagem.");
+            Console.WriteLine("0 - SAIR");
+            return int.Parse(Console.ReadLine());
         }
     }
 }
